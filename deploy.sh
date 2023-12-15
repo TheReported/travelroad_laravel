7acfe452c146aa97dbc614be865f0f6cc5fe64be
TheReported <diegopca12@gmail.com>
2023-12-15 20:06:17 +0000

    U

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+REMOTE_USER="diego"
+REMOTE_HOST="diego.arkania.es"
+REPO_URL="git@github.com:TheReported/travelroad_laravel.git"
+TARGET_DIR="travelroad_laravel"
+
+# Conexión SSH y clonar el repositorio
+ssh "$REMOTE_USER@$REMOTE_HOST" "
+  if [ ! -d $TARGET_DIR ]; then
+    echo 'Clonando el repositorio...'
+    git clone $REPO_URL $TARGET_DIR
+    cd $TARGET_DIR
+    echo 'Realizando composer install...'
+    composer install
+  else
+    echo 'El directorio ya existe.'
+    cd $TARGET_DIR
+  fi
+"
